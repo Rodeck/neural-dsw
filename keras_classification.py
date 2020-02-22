c13f8d9d4e69a912a69613465e43eff567115cb3
Pajak <gabriel.pajak@god-se.com>
2020-02-22 18:26:22 +0100

    Add dataset, gitignore.

--- keras_classification.py
+++ keras_classification.py
@@ -19,8 +19,9 @@ x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_
 # create model, add dense layers one by one specifying activation function
 model = Sequential()
 model.add(Dense(15, input_dim=8, activation='relu')) # input layer requires input_dim param
-model.add(Dense(10, activation='relu'))
+model.add(Dense(13, activation='relu'))
 model.add(Dense(8, activation='relu'))
+model.add(Dense(4, activation='relu'))
 model.add(Dropout(.2))
 model.add(Dense(1, activation='sigmoid')) # sigmoid instead of relu for final probability between 0 and 1
 
@@ -28,7 +29,7 @@ model.add(Dense(1, activation='sigmoid')) # sigmoid instead of relu for final pr
 model.compile(loss="binary_crossentropy", optimizer="adam", metrics=['accuracy'])
 
 # call the function to fit to the data (training the network)
-model.fit(x_train, y_train, epochs = 1000, batch_size=20, validation_data=(x_test, y_test))
+model.fit(x_train, y_train, epochs = 100, batch_size=20, validation_data=(x_test, y_test))
 
 # save the model
 model.save('weights.h5')
